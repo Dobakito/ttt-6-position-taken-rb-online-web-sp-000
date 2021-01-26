15edcf47a9dfaecc4d6fd1b7bb2800ba59946d22
Michael Dobak <mdobak44@icloud.com>
2021-01-26 00:01:57 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,5 +3,6 @@ def position_taken?(board, index)
   if board[index] != " " or board[index] != "" or board[index] != "nil"
     false
   else board[index] == "X" or board[index] == "O"
+    true
 end
 end
